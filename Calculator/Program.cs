8226be0e93a9e586a0c2d3dda78361ec8ed07c8a
b01018981 <b01018981@M-MXB105C41>
2026-09-22 11:34:17 +0100

    Implemented Addition program.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type in the first integer number followed by the enter key.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type in the second integer number followed by the enter key.");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} results in {2}.", firstNumber, secondNumber, result);
