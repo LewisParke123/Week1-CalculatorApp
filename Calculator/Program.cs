c5019a1673cbe903705ba391860b9b15522460e7
LewisParke123 <lewisparke123@gmail.com>
2026-09-22 11:53:32 +0100

    Added if-else statements to Program.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,49 @@
-﻿Console.WriteLine("Type in the first integer number followed by the enter key.");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Type in the second integer number followed by the enter key.");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+﻿CalculatorApp();
 
-int result = firstNumber + secondNumber;
+void CalculatorApp()
+{
 
-Console.WriteLine("Adding {0} and {1} results in {2}.", firstNumber, secondNumber, result);
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
+
+    Console.WriteLine("Type in the first integer number followed by the enter key.");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Type in the second integer number followed by the enter key.");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose the operation from the following list:");
+    Console.WriteLine("1 = addition");
+    Console.WriteLine("2 = subtraction");
+    Console.WriteLine("3 = multiplication");
+    Console.WriteLine("4 = division");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} and {secondNumber} results in {result}.");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {secondNumber} from {firstNumber} results in {result}.");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} and {secondNumber} results in {result}.");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} by {secondNumber} results in {result}.");
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice.");
+        return;
+    }
+}
